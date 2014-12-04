5391b51d46eb2e84237818a33e3b18889b8ec904
talregev <tal.regev@gmail.com>
2014-12-04 13:02:17 +0200

    uodate inastall-android-sdk.sh

--- install/install-android-sdk.sh
+++ install/install-android-sdk.sh
@@ -2,23 +2,23 @@
 
 #from https://developer.android.com/sdk/installing/index.html?pkg=tools
 #install sdk 
+SDK_NAME="Sdk"
 APP_DIR=~/Android
-Sdk_Name="Sdk"
 cd ~/Downloads
 wget -O android-sdk.tgz 'https://dl.google.com/android/android-sdk_r23.0.2-linux.tgz'
+rm -rf ${APP_DIR}/$SDK_NAME
 mkdir -p ${APP_DIR}
 tar -xvzf android-sdk.tgz -C ${APP_DIR}
-rm -rf ${APP_DIR}/$Sdk_Name/
 cd ${APP_DIR}
-mv android-sdk-linux/ $Sdk_Name/
+mv android-sdk-linux/ $SDK_NAME/
 rm -f ~/Downloads/android-sdk.tgz
 
 #Add android-studio and sdk to your PATH
-echo "export PATH=\$PATH:${APP_DIR}/$Sdk_Name/tools:${APP_DIR}/$Sdk_Name/platform-tools:/opt/android-studio/bin">> ~/.bashrc
-echo "export ANDROID_HOME=${APP_DIR}/$Sdk_Name">> ~/.bashrc
+echo "export PATH=\$PATH:${APP_DIR}/$SDK_NAME/tools:${APP_DIR}/$SDK_NAME/platform-tools:/opt/android-studio/bin">> ~/.bashrc
+echo "export ANDROID_HOME=${APP_DIR}/$SDK_NAME">> ~/.bashrc
 
-export PATH=$PATH:${APP_DIR}/$Sdk_Name/tools:${APP_DIR}/$Sdk_Name/platform-tools:/opt/android-studio/bin
-export ANDROID_HOME=${APP_DIR}/$Sdk_Name
+export PATH=$PATH:${APP_DIR}/$SDK_NAME/tools:${APP_DIR}/$SDK_NAME/platform-tools:/opt/android-studio/bin
+export ANDROID_HOME=${APP_DIR}/$SDK_NAME
 
 #from http://stackoverflow.com/questions/17963508/how-to-install-android-sdk-build-tools-on-the-command-line
 #add package to android sdk
