39ab5b4e83ee02d7d3263c043f4357bc28c080c8
talregev <tal.regev@gmail.com>
2015-01-02 22:08:34 +0200

    update android core install script

--- install/install-android-camera-driver.sh
+++ install/install-android-camera-driver.sh
@@ -2,14 +2,10 @@
 
 PROJECT_DIR=~/ANPL
 
-# clone android_core , android_extras
-cd ${PROJECT_DIR}/rosjava/src
-git clone -b indigo https://github.com/rosjava/android_core.git
-git clone -b indigo https://github.com/rosjava/android_extras.git
-
+install-android-core.sh
 install-android-opencv.sh
 
-cd android_core
+cd ${PROJECT_DIR}/rosjava/src/android_core
 echo "include \"android_camera_driver\"">> settings.gradle
 
 #clone android_sensor_driver
