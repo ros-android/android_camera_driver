d52ec0fce3a9091dcb0f03354b63f8a24d969ab5
talregev <tal.regev@gmail.com>
2015-01-11 15:18:24 +0200

    update to the latest app:  - rewrite the mechanism for opencv listner, and read camera picture.  - now it at full screen and more cool.  - the screen is never get off when the app is running.  - add swap camera, if it more than 1 camera on device.  - add flip picture if it front camera (to view, and send it normal to user).  - preserve user choosing color: RGB, Gray color, Canny edge.  - preserve user can choose the compression for Jpeg and PNG, and None.  - now none compresstion is partly working.  - add robot name to UI, and logic. user can now choose robot name.  - rearrange the code, and fix bugs.

--- src/org/ros/android/android_camera_driver/CameraPublisher.java
+++ src/org/ros/android/android_camera_driver/CameraPublisher.java
@@ -29,91 +29,63 @@
 
 package org.ros.android.android_camera_driver;
 
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.hardware.Camera;
 import android.util.Log;
 
-import org.opencv.android.BaseLoaderCallback;
-import org.opencv.android.LoaderCallbackInterface;
-import org.opencv.android.OpenCVLoader;
+import org.jboss.netty.buffer.ChannelBufferOutputStream;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.core.MatOfInt;
+import org.opencv.highgui.Highgui;
+import org.opencv.imgproc.Imgproc;
+import org.ros.internal.message.MessageBuffers;
+import org.ros.message.Time;
 import org.ros.namespace.GraphName;
+import org.ros.namespace.NameResolver;
 import org.ros.node.ConnectedNode;
 import org.ros.node.Node;
 import org.ros.node.NodeMain;
+import org.ros.node.topic.Publisher;
+
+import java.nio.ByteBuffer;
+
+import sensor_msgs.CameraInfo;
+import sensor_msgs.CompressedImage;
+import sensor_msgs.Image;
+
+import static org.opencv.core.Core.flip;
+
 
 /**
  * @author chadrockey@gmail.com (Chad Rockey)
  * @author axelfurlan@gmail.com (Axel Furlan)
+ * @author tal.regev@gmail.com  (Tal Regev)
  */
 
-public class CameraPublisher implements NodeMain {
+public class CameraPublisher implements NodeMain, CvCameraViewListener2 {
     private static final String TAG = "SENSORS::CameraPublisher";
-    private Sample2View mView;
-    private Activity mainActivity;
-    private ConnectedNode node = null;
-
-    private BaseLoaderCallback mOpenCVCallBack = new BaseLoaderCallback(mainActivity) {
-        @SuppressWarnings("deprecation")
-        @Override
-        public void onManagerConnected(int status) {
-            Log.i(TAG, "onManagerConnected 1");
-
-            switch (status) {
-                case LoaderCallbackInterface.SUCCESS: {
-                    // Create and set View
-                    mView = new Sample2View(mainActivity, node);
-                    mainActivity.setContentView(mView);
-
-                    // Check native OpenCV camera
-                    if (!mView.openCamera()) {
-                        AlertDialog ad = new AlertDialog.Builder(mainActivity).create();
-                        ad.setCancelable(false); // This blocks the 'BACK' button
-                        ad.setMessage("Fatal error: can't open camera!");
-                        ad.setButton("OK", new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.dismiss();
-                                mainActivity.finish();
-                            }
-                        });
-                        ad.show();
-                    }
-                }
-                break;
-                default: {
-                    Log.i(TAG, "OpenCV loading FAIL!!!");
-                    super.onManagerConnected(status);
-                }
-                break;
-            }
-        }
-    };
 
 
-    public CameraPublisher(Activity mainAct) {
-        this.mainActivity = mainAct;
-    }
-
+    protected ConnectedNode node = null;
+    protected Publisher<CompressedImage> imagePublisher;
+    protected Publisher<sensor_msgs.Image> rawImagePublisher;
+    protected Publisher<sensor_msgs.CameraInfo> cameraInfoPublisher;
+    protected sensor_msgs.CameraInfo cameraInfo;
+    protected int counter;
     @SuppressWarnings("deprecation")
-    public void resume() {
-        if ((null != mView) && !mView.openCamera()) {
-            AlertDialog ad1 = new AlertDialog.Builder(mainActivity).create();
-            ad1.setCancelable(false); // This blocks the 'BACK' button
-            ad1.setMessage("Fatal error: can't open camera!");
-            ad1.setButton("OK", new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int which) {
-                    dialog.dismiss();
-                    mainActivity.finish();
-                }
-            });
-            ad1.show();
-        }
+    protected Camera.CameraInfo info = new Camera.CameraInfo();
+    @SuppressWarnings("deprecation")
+    protected final int FRONT = Camera.CameraInfo.CAMERA_FACING_FRONT;
+    public String robotName;
+
+
+    public CameraPublisher() {
     }
 
-    public void releaseCamera() {
-        if (null != mView)
-            mView.releaseCamera();
-    }
 
     public GraphName getDefaultNodeName() {
         return GraphName.of("android_camera_driver/cameraPublisher");
@@ -124,19 +96,156 @@ public class CameraPublisher implements NodeMain {
 
     public void onStart(final ConnectedNode node) {
         this.node = node;
-
-        if (!OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_2, this.mainActivity, mOpenCVCallBack)) {
-            Log.e(TAG, "Cannot connect to OpenCV Manager");
-        }
+        NameResolver resolver;
+        resolver = node.getResolver().newChild(robotName + "/camera");
+        imagePublisher = node.newPublisher(resolver.resolve("image/compressed"), CompressedImage._TYPE);
+        cameraInfoPublisher = node.newPublisher(resolver.resolve("camera_info"), CameraInfo._TYPE);
+        rawImagePublisher = node.newPublisher(resolver.resolve("image/raw"), Image._TYPE);
+        Log.i(TAG, "called onStart");
     }
 
-    //@Override
+    @Override
     public void onShutdown(Node arg0) {
+
     }
 
-    //@Override
+    @Override
     public void onShutdownComplete(Node arg0) {
     }
 
-}
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+    }
 
+    @Override
+    public void onCameraViewStopped() {
+    }
+
+
+    @Override
+    public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+        Mat         frame       = null;
+        Mat         frameToSend = new Mat();
+        MatOfByte   buf         = new MatOfByte();
+        MatOfInt    params;
+
+        counter = (counter + 1) % 7;
+        ChannelBufferOutputStream stream = new ChannelBufferOutputStream(MessageBuffers.dynamicBuffer());
+        switch (MainActivity.viewMode) {
+            case MainActivity.VIEW_MODE_RGBA:
+                frame = inputFrame.rgba();
+                break;
+            case MainActivity.VIEW_MODE_GRAY:
+            case MainActivity.VIEW_MODE_CANNY:
+                frame = inputFrame.gray();
+                break;
+        }
+        if (null != node && null != frame) {
+
+            //noinspection deprecation
+            android.hardware.Camera.getCameraInfo(MainActivity.mCameraId, info);
+            if (info.facing == FRONT)
+            {
+                flip(frame, frame,1);
+            }
+
+            switch (MainActivity.viewMode) {
+                case MainActivity.VIEW_MODE_RGBA:
+                    Imgproc.cvtColor(frame, frameToSend, Imgproc.COLOR_RGBA2BGR);
+                    break;
+                case MainActivity.VIEW_MODE_GRAY:
+                    frameToSend = frame;
+                    break;
+                case MainActivity.VIEW_MODE_CANNY:
+                    Imgproc.Canny(frame, frame, 80, 100);
+                    frameToSend = frame;
+                    break;
+            }
+
+            Time currentTime = node.getCurrentTime();
+
+            int cols = frame.cols();
+            int rows = frame.rows();
+
+            try {
+
+                cameraInfo = cameraInfoPublisher.newMessage();
+                cameraInfo.getHeader().setFrameId("camera");
+                cameraInfo.getHeader().setStamp(currentTime);
+                cameraInfo.setWidth(cols);
+                cameraInfo.setHeight(rows);
+                cameraInfoPublisher.publish(cameraInfo);
+
+
+                if (MainActivity.imageCompression >= MainActivity.IMAGE_TRANSPORT_COMPRESSION_PNG)
+                {
+                    //Compressed image
+                    CompressedImage image = imagePublisher.newMessage();
+
+                    image.getHeader().setStamp(currentTime);
+                    image.getHeader().setFrameId("camera");
+
+                    switch (MainActivity.imageCompression)
+                    {
+                        case MainActivity.IMAGE_TRANSPORT_COMPRESSION_PNG:
+                            image.setFormat("png");
+                            params = new MatOfInt(Highgui.IMWRITE_PNG_COMPRESSION, MainActivity.imageJPEGCompressionQuality);
+                            Highgui.imencode(".png", frameToSend, buf, params);
+                            break;
+                        case MainActivity.IMAGE_TRANSPORT_COMPRESSION_JPEG:
+                            image.setFormat("jpeg");
+                            params = new MatOfInt(Highgui.IMWRITE_JPEG_QUALITY, MainActivity.imageJPEGCompressionQuality);
+                            Highgui.imencode(".jpg", frameToSend, buf, params);
+                            break;
+
+                    }
+
+                    stream.write(buf.toArray());
+                    image.setData(stream.buffer().copy());
+                    imagePublisher.publish(image);
+                }
+                else if (counter == 0)
+                {
+                    // Raw image
+                    Log.i(TAG, "Raw image 1");
+
+
+                    Bitmap bmp = Bitmap.createBitmap(cols, rows, Bitmap.Config.ARGB_8888);
+                    Utils.matToBitmap(frame, bmp);
+
+
+                    Image rawImage = rawImagePublisher.newMessage();
+                    rawImage.getHeader().setStamp(currentTime);
+                    rawImage.getHeader().setFrameId("camera");
+                    rawImage.setEncoding("rgba8");
+                    rawImage.setWidth(cols);
+                    rawImage.setHeight(rows);
+                    rawImage.setStep(bmp.getRowBytes());
+
+                    ByteBuffer bb = ByteBuffer.allocate(bmp.getRowBytes() * bmp.getHeight());
+
+
+                    bmp.copyPixelsToBuffer(bb);
+                    stream.write(bb.array());
+                    rawImage.setData(stream.buffer().copy());
+                    rawImagePublisher.publish(rawImage);
+
+                    /*
+                    params = new MatOfInt(Highgui.CV_IMWRITE_PXM_BINARY, 1);
+                    Highgui.imencode(".bmp", frameToSend, buf, params);
+                    stream.write(buf.toArray());
+                    rawImage.setData(stream.buffer().copy());
+                    rawImagePublisher.publish(rawImage);
+                    stream.buffer().clear();
+                    */
+                    Log.i(TAG, "Raw image 6");
+                }
+            }
+            catch (Exception e)
+            {
+                Log.e(TAG, "Frame conversion and publishing throws an exception: " + e.getMessage());
+            }
+        }
+        return frame;
+    }
+}
--- src/org/ros/android/android_camera_driver/MainActivity.java
+++ src/org/ros/android/android_camera_driver/MainActivity.java
@@ -16,40 +16,84 @@
 
 package org.ros.android.android_camera_driver;
 
+import android.content.Intent;
+import android.hardware.Camera;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.SubMenu;
+import android.view.SurfaceView;
 import android.view.Window;
 import android.view.WindowManager;
 
+import com.google.common.base.Preconditions;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
 import org.ros.address.InetAddressFactory;
 import org.ros.android.RosActivity;
+import org.ros.exception.RosRuntimeException;
 import org.ros.node.NodeConfiguration;
 import org.ros.node.NodeMainExecutor;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * @author chadrockey@gmail.com (Chad Rockey)
  * @author axelfurlan@gmail.com (Axel Furlan)
+ * @author tal.regev@gmail.com  (Tal Regev)
  */
 
 
 public class MainActivity extends RosActivity {
+    private static final String TAG = "android_camera_driver::MainActivity";
 
-    public static final int VIEW_MODE_RGB = 0;
-    public static int viewMode = VIEW_MODE_RGB;
+    public static final int VIEW_MODE_RGBA = 0;
     public static final int VIEW_MODE_GRAY = 1;
     public static final int VIEW_MODE_CANNY = 2;
     public static final int IMAGE_TRANSPORT_COMPRESSION_NONE = 0;
     public static final int IMAGE_TRANSPORT_COMPRESSION_PNG = 1;
     public static final int IMAGE_TRANSPORT_COMPRESSION_JPEG = 2;
+
+
+    public static int viewMode = VIEW_MODE_RGBA;
     public static int imageCompression = IMAGE_TRANSPORT_COMPRESSION_JPEG;
-    // OpenCV Camera
-    private static final String TAG = "SENSORS::MainActivity";
+
     public static int imageJPEGCompressionQuality = 80;
-    public static int imagePNGCompressionQuality = 80;
-    private CameraPublisher cam_pub;
+    public static int imagePNGCompressionQuality = 3;
+
+    public static int mCameraId = 0;
+
+    private final int MASTER_CHOOSER_REQUEST_CODE = 1;
+    private CameraBridgeViewBase mOpenCvCameraView;
+    private CameraPublisher cam_pub = new CameraPublisher();
+    private Boolean isInit = false;
+    private String robotName;
+
+    @SuppressWarnings("deprecation")
+    protected final int numberOfCameras = Camera.getNumberOfCameras();
+
+    protected BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS: {
+                    Log.i(TAG, "OpenCV loaded successfully");
+                    mOpenCvCameraView.enableView();
+                }
+                break;
+                default: {
+                    super.onManagerConnected(status);
+                }
+                break;
+            }
+        }
+    };
 
 
     public MainActivity() {
@@ -57,51 +101,103 @@ public class MainActivity extends RosActivity {
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        if (cam_pub != null) {
-            cam_pub.releaseCamera();
-        }
-    }
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.main);
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.HelloOpenCvView);
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(cam_pub);
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
+    public void startMasterChooser() {
+        Preconditions.checkState(getMasterUri() == null);
+        // Call this method on super to avoid triggering our precondition in the
+        // overridden startActivityForResult().
+        Intent intent = new Intent(this, MasterChooser.class);
+        super.startActivityForResult(intent, MASTER_CHOOSER_REQUEST_CODE);
+    }
 
-        if (cam_pub != null) {
-            cam_pub.resume();
-        } else
-            Log.i(TAG, "Error while resuming app");
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == MASTER_CHOOSER_REQUEST_CODE) {
+                if (data.getBooleanExtra("ROS_MASTER_CREATE_NEW", false)) {
+                    nodeMainExecutorService.startMaster(data.getBooleanExtra("ROS_MASTER_PRIVATE", true));
+                }
+                else {
+                    URI uri;
+                    try {
+                        uri = new URI(data.getStringExtra("ROS_MASTER_URI"));
+                        robotName = data.getStringExtra("ROBOT_NAME");
+                    }
+                    catch (URISyntaxException e) {
+                        throw new RosRuntimeException(e);
+                    }
+                    nodeMainExecutorService.setMasterUri(uri);
+                }
+                // Run init() in a new thread as a convenience since it often requires network access.
+                new AsyncTask<Void, Void, Void>() {
+                    @Override
+                    protected Void doInBackground(Void... params) {
+                        MainActivity.this.init(nodeMainExecutorService);
+                        return null;
+                    }
+                }.execute();
+            }
+            else {
+                // Without a master URI configured, we are in an unusable state.
+                nodeMainExecutorService.shutdown();
+            }
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (isInit) {
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_9, this, mLoaderCallback);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
         SubMenu subPreview = menu.addSubMenu("Color settings");
-        subPreview.add(1, VIEW_MODE_RGB, 0, "RGB Color").setChecked(true);
+        subPreview.add(1, VIEW_MODE_RGBA, 0, "RGB Color").setChecked(true);
         subPreview.add(1, VIEW_MODE_GRAY, 0, "Gray Scale");
         subPreview.add(1, VIEW_MODE_CANNY, 0, "Canny edges");
         subPreview.setGroupCheckable(1, true, true);
 
         SubMenu subCompression = menu.addSubMenu("Compression");
-        //subCompression.add(2,IMAGE_TRANSPORT_COMPRESSION_NONE,0,"None");
+        subCompression.add(2,IMAGE_TRANSPORT_COMPRESSION_NONE,0,"None");
         SubMenu subPNGCompressionRate = subCompression.addSubMenu(2, IMAGE_TRANSPORT_COMPRESSION_PNG, 0, "Png");
         subPNGCompressionRate.setHeaderTitle("Compression quality");
         subPNGCompressionRate.getItem().setChecked(true);
-        subPNGCompressionRate.add(4, 50, 0, "50");
-        subPNGCompressionRate.add(4, 60, 0, "60");
-        subPNGCompressionRate.add(4, 70, 0, "70");
-        subPNGCompressionRate.add(4, 80, 0, "80").setChecked(true);
-        subPNGCompressionRate.add(4, 90, 0, "90");
-        subPNGCompressionRate.add(4, 100, 0, "100");
+        subPNGCompressionRate.add(4, 3, 0, "3").setChecked(true);
+        subPNGCompressionRate.add(4, 4, 0, "4");
+        subPNGCompressionRate.add(4, 5, 0, "5");
+        subPNGCompressionRate.add(4, 6, 0, "6");
+        subPNGCompressionRate.add(4, 7, 0, "7");
+        subPNGCompressionRate.add(4, 8, 0, "8");
+        subPNGCompressionRate.add(4, 9, 0, "9");
         subPNGCompressionRate.setGroupCheckable(4, true, true);
 
         SubMenu subJPEGCompressionRate = subCompression.addSubMenu(2, IMAGE_TRANSPORT_COMPRESSION_JPEG, 0, "Jpeg");
@@ -115,6 +211,10 @@ public class MainActivity extends RosActivity {
         subJPEGCompressionRate.add(3, 90, 0, "90");
         subJPEGCompressionRate.add(3, 100, 0, "100");
         subJPEGCompressionRate.setGroupCheckable(3, true, true);
+
+        if (numberOfCameras > 1) {
+            menu.addSubMenu(5, 0, 0, "Swap camera");
+        }
 
         return super.onCreateOptionsMenu(menu);
     }
@@ -138,16 +238,32 @@ public class MainActivity extends RosActivity {
                 imagePNGCompressionQuality = item.getItemId();
                 item.setChecked(true);
                 break;
+            case 5:
+                swapCamera();
+                break;
         }
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
+    //from http://stackoverflow.com/questions/16273370/opencvandroidsdk-switching-between-front-camera-and-back-camera-at-run-time
+    public void swapCamera() {
+        mCameraId = (mCameraId + 1) % numberOfCameras;
+        mOpenCvCameraView.disableView();
+        mOpenCvCameraView.setCameraIndex(mCameraId);
+        mOpenCvCameraView.enableView();
+    }
+
+
     protected void init(NodeMainExecutor nodeMainExecutor) {
         NodeConfiguration nodeConfiguration = NodeConfiguration.newPublic(InetAddressFactory.newNonLoopback().getHostAddress());
+
         nodeConfiguration.setMasterUri(getMasterUri());
         nodeConfiguration.setNodeName("android_camera_driver");
-        this.cam_pub = new CameraPublisher(this);
-        nodeMainExecutor.execute(this.cam_pub, nodeConfiguration);
+
+        nodeMainExecutor.execute(cam_pub, nodeConfiguration);
+        cam_pub.robotName = robotName;
+        Log.i(TAG, "called nodeMainExecutor");
+        isInit = true;
+        onResume();
     }
 }
